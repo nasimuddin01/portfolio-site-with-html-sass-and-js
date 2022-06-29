65cfc5931d99a47beb589ec670c9a22b1e3a2cdf
Akramsarker <dev.mdakram@gmail.com>
2022-06-29 22:11:12 +0600

    Add footer section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 new fullpage("#fullpage", {
   licenseKey: "D3E291AE-D65447D5-84602E0F-3F7962D0",
-  sectionsColor: ["#000000", "#000000", "#000000", "#000000", "#000000", "#000000", "#000000"],
+  sectionsColor: [
+    "#000000",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#000000",
+    "#000000",
+  ],
   navigation: true,
   navigationPosition: "right",
   navigationTooltips: [
